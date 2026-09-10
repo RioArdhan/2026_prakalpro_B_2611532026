73d9411441b57b608fae0e7de8bc77497e10784c
RioArdhan <rioardhanm@gmail.com>
2026-09-10 15:12:59 +0700

    Prak AlproB 2026

---
+++ Pekan 1/Tugas1_2611532026.py
@@ -0,0 +1,16 @@
+# Buatlah sebuah file program python bebas dengan nama tugas1_nim.py
+# Program menghitung luas dan keliling persegi panjang
+
+print("=== PROGRAM PERSEGI PANJANG ===")
+
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+print("\nHasil Perhitungan")
+print("Panjang  :", panjang)
+print("Lebar    :", lebar)
+print("Luas     :", luas)
+print("Keliling :", keliling)
