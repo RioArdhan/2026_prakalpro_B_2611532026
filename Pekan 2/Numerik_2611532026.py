2230f9325a064578d8c47b29e21cf73a46c33ec8
RioArdhan <rioardhanm@gmail.com>
2026-09-11 14:51:52 +0700

    Prak AlproB 2026

--- Pekan 2/Numerik_2611532026.py
+++ Pekan 2/Numerik_2611532026.py
@@ -1,13 +1,13 @@
 # Buat file dengan nama Numerik_2611532026.py
-# nama variabel ditambah 4 digit terakhir NIM contoh: a_2026
+# nama variabel ditambah 4 digit terakhir NIM contoh: a_1234
 #create variabel a variabel with numerik type value.
-a=100
-print("The type of variabel having value", a, " is ", type(a))
+a_2026 = 100
+print("The type of variabel having value", a_2026, " is ", type(a_2026))
 
 #create a variabel with float value.
-b=10.2345
-print("The type of variabel having value", b, " is ", type (b))
+b_2026 = 10.2345
+print("The type of variabel having value", b_2026, " is ", type (b_2026))
 
 #create a variabel with complex value.
-c=100+3j
-print("The type of variabel having value", c, " is", type (c))
+c_2026 = 100 + 3j
+print("The type of variabel having value", c_2026, " is", type (c_2026))
